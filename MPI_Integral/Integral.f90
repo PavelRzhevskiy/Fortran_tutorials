04bb5fcdc5c22fb9beeaa33d4c8c1ba6ded49b76
Pavel Rzhevskiy <rzhevskiyp@gmail.com>
2023-09-07 22:05:05 +0600

    Add std. dev. for times and removed extra code

--- MPI_Integral/Integral.f90
+++ MPI_Integral/Integral.f90
@@ -4,13 +4,12 @@ program integral
 
   
   integer :: ierr, myrank, mysize
-  integer :: q, r, arr_size, N
+  integer :: q, r, x_size, N
   integer :: i,j,k, start_index
   real(kind=8) :: h, sum, global_sum, x0, x, y
-  real(kind=8) :: t_start, t_stop
-  real(kind=8), allocatable :: sum_arr(:)
+  real(kind=8) :: t_start, t_stop, t_sum, t_sum_sq
 
-
+  
   call MPI_INIT(ierr)
 
   ! define # of pts and grid step 
@@ -20,28 +19,24 @@ program integral
   call MPI_COMM_RANK(MPI_COMM_WORLD, myrank, ierr)
   call MPI_COMM_SIZE(MPI_COMM_WORLD, mysize, ierr)
 
-  !tick starting time for process 0 and allocate array for holding sums
-  if (myrank == 0) then
-     t_start = MPI_WTIME()
-     allocate(sum_arr(mysize))
-  end if
-     
+       
   ! Get the size of x-sliced chunk for the process
   q = N/mysize
   r = N - q*mysize
   if (myrank + 1 <= r) then
-     arr_size = q + 1
+     x_size = q + 1
      start_index = (q + 1)*myrank
   else
-     arr_size = q
+     x_size = q
      start_index = q*myrank + r
   end if
   
   !Calculate local sum
   sum = 0
   x0 = start_index*h
+  t_start = MPI_WTIME()
   !print*, "rank = ", myrank, "start_index = ", start_index, "arr size = ", arr_size
-  do i = 0, arr_size-1
+  do i = 0, x_size-1
      x = x0 + i*h
 
      do j = 0, N-1
@@ -55,16 +50,19 @@ program integral
      end do
      
   end do
+  t_stop = MPI_WTIME()
   sum = sum*h**3
   print*, "myrank = ", myrank,  "sum =", sum
   
   
   
   call MPI_REDUCE(sum, global_sum, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr) 
+  call MPI_REDUCE(t_stop - t_start, t_sum, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr) 
+  call MPI_REDUCE((t_stop - t_start)**2, t_sum_sq, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr) 
+  
   if (myrank == 0) then
-     t_stop = MPI_WTIME()
      print*, "Integral = ", global_sum, "error ~ ", h**2
-     print*, "Elapsed time = ", t_stop - t_start
+     print*, "Average elapsed time = ", t_sum/mysize, " +- ", sqrt(t_sum_sq/mysize - (t_sum/mysize)**2)
   end if
   
   call MPI_FINALIZE(ierr)
@@ -98,7 +96,7 @@ contains
        if (myrank == 0) then 
           x_plane = (i == 1)
        else
-          x_plane = (i == arr_size)
+          x_plane = (i == x_size)
        end if
 
        ! if only one process is involved
--- MPI_Integral/script.sh
+++ MPI_Integral/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 np_arr=(1 2 4 8 16)
-exe=release/integral_release
+exe=release
 output="sample_output.dat"
 echo "Program output" > ${output}
 
@@ -10,4 +10,5 @@ do
     echo "np = " ${np_arr[$i]} >> ${output}
     mpirun --oversubscribe -np ${np_arr[$i]} ${exe} >> ${output}
     echo "" >> ${output}
+    sleep 5
 done
